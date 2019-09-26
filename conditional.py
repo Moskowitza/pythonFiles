de87fcbd66d31a0e9dd916465a97773078a51217
moskowitza <moskowitzaaron@mac.com>
2019-09-26 08:48:29 -0400

    some conditional notes

--- conditional.py
+++ conditional.py
@@ -1,19 +1,36 @@
-language = 'Javascript'
-secondLang = 'Python'
-# No Switch case, just use elif
-if language == 'Javascript':
-    print("Javascript the best")
-elif language == 'Python':
-    print("Python best Language")
-elif language == 'JAVA':
-    print("Really, JAVA?")
-else:
-    print("that's ok")
+# language = 'Javascript'
+# secondLang = 'Python'
+# # No Switch case, just use elif
+# if language == 'Javascript':
+#     print("Javascript the best")
+# elif language == 'Python':
+#     print("Python best Language")
+# elif language == 'JAVA':
+#     print("Really, JAVA?")
+# else:
+#     print("that's ok")
 
-user = 'Admin'
-logged_in = False
-# You can use the 'and' keyword to check fall through or use 'or' to check for one
-if user == "Admin" or logged_in:
-    print('Welcome to Admin Page')
+# user = 'Admin'
+# logged_in = False
+# # You can use the 'and' keyword to check fall through or use 'or' to check for one
+# if user == "Admin" or logged_in:
+#     print('Welcome to Admin Page')
+# else:
+#     print('Sorry, not admin')
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# c = b
+# print(id(a), id(b), id(c))
+# print(a == b)
+# print(a is b)
+# print(b is c)
+
+# Falsy=False, None, 0, empty string'', set[], tuple() or dict{}
+# truthy=True, nonzero number
+
+condition = None
+if condition:
+    print('This is based on facts')
 else:
-    print('Sorry, not admin')
+    print("It's fiction")
